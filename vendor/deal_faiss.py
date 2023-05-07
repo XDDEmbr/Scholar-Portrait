75ac3d9de609e9569f668e48b0bac5b0da456156
XDDEmbr <91446912+XDDEmbr@users.noreply.github.com>
2023-05-07 21:57:27 +0800

    Delete vendor directory

--- vendor/deal_faiss.py
+++
@@ -1,9 +0,0 @@
-import sys
-import tarfile
-
-# Extract faiss_package.tar.gz to vendor/faiss_package directory
-with tarfile.open("vendor/faiss_package.tar.gz", "r:gz") as tar:
-    tar.extractall("vendor/")
-
-# Add vendor/faiss_package directory to Python path
-sys.path.append("vendor/faiss_package")
